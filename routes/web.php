1d2ac1234a6c18b2a821307d04ce3029006269d5
usmans-github <usmanali730771@gmail.com>
2025-01-25 21:14:39 +0500

    add role in user table

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [AuthController::class, 'create']);
 
 
-Route::get('admin/create', [AdminController::class, 'create']);
-
-
 
 
 Route::middleware('auth')->group(function ()  {
