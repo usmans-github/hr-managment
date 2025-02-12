773aab543f5f57c42d57631b771895e27b21c007
usmans-github <usmanali730771@gmail.com>
2025-02-12 21:46:49 +0500

    Refactor employee migration for improved readability and consistency

--- database/migrations/2025_01_16_095247_create_employees_table.php
+++ database/migrations/2025_01_16_095247_create_employees_table.php
@@ -1,40 +1,40 @@
-<?php
+    <?php
 
-use App\Models\Department;
-use App\Models\Position;
-use App\Models\User;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use App\Models\Department;
+    use App\Models\Position;
+    use App\Models\User;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    return new class extends Migration
     {
-        Schema::create('employees', function (Blueprint $table) {
-            $table->id();   
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->string('first_name')->unique();
-            $table->string('last_name')->unique();
-            $table->string('email');
-            $table->string('password');
-            $table->string('phone');
-            $table->foreignIdFor(Position::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignIdFor(Department::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->string('join_date');    
-            $table->string('salary');
-            $table->timestamps();
-        });
-    }
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            Schema::create('employees', function (Blueprint $table) {
+                $table->id();   
+                $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+                $table->string('first_name');
+                $table->string('last_name')->unique();
+                $table->string('email');
+                $table->string('password');
+                $table->string('phone');
+                $table->foreignIdFor(Position::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+                $table->foreignIdFor(Department::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+                $table->string('join_date');    
+                $table->string('salary');
+                $table->timestamps();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('employees');
-    }
-};
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('employees');
+        }
+    };
