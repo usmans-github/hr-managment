faee1b9f861e97d694d2e376e672ea76c72468f9
usmans-github <usmanali730771@gmail.com>
2025-02-21 01:44:25 +0500

    Update command name for checking absent employees in the daily schedule

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('check-absent-employees')->dailyAt('19:00');
+Schedule::command('app:check-absent-employees')->dailyAt('19:00');
