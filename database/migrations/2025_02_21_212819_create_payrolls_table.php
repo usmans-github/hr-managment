778b7913dd5b5ce660195131fcd0463b3b303ae1
usmans-github <usmanali730771@gmail.com>
2025-02-21 21:33:05 +0500

    Add Payroll model and migration for payrolls table

---
+++ database/migrations/2025_02_21_212819_create_payrolls_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->date('pay_period');
+            $table->decimal('amount', 10, 2);
+            $table->enum('status', ['Pending', 'Paid'])->default('Pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payrolls');
+    }
+};
